efdea59bbc0547330bf54af2a992f5b6696819af
lihao <lihao@nway.com.cn>
2023-01-27 17:06:10 +0800

    增加配置文件，以及增加对存储规则进行解析

--- udp_server/main.go
+++ udp_server/main.go
@@ -5,19 +5,41 @@ import (
 	"math/rand"
 	"net"
 	"os"
+	"path/filepath"
+	"runtime"
 	"strconv"
 	"strings"
 	"time"
 
+	"github.com/astaxie/beego/config"
 	_ "github.com/zenwerk/go-wave"
 )
 
 var Sessions map[string]Rst_session
 var Files map[uint16]*os.File
+var Save_dir string
+var Save_rule string
+var Ext_name string
+var Use_caller bool
+var Use_callee bool
+var Use_uuid bool
 
-//起始协议包为 INV:caller:callee:uuid:caller_port:callee_port
-//结束协议包为 BYE:uuid
+//起始协议包为 INV :caller:callee:uuid:caller_port:callee_port
+//结束协议包为 BYE :uuid
 //数据协议包为 DATA:port:payload:data
+func GetCurrentDirectory() string {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		println(err)
+	}
+	if runtime.GOOS == "linux" {
+		dir += "/"
+	} else {
+		dir += "\\"
+	}
+	return dir
+
+}
 func ByteString(p []byte) string {
 	for i := 0; i < len(p); i++ {
 		if p[i] == 0 {
@@ -39,8 +61,13 @@ func random(min, max int) int {
 }
 
 func main() {
-
-	PORT := ":9527"
+	conf_file := GetCurrentDirectory() + "udp_server.conf"
+	iniconf, err := config.NewConfig("ini", conf_file)
+	if err != nil {
+		fmt.Println("load config file failed")
+		return
+	}
+	PORT := iniconf.String("udp_port")
 	//os.Cre
 	s, err := net.ResolveUDPAddr("udp4", PORT)
 	if err != nil {
@@ -55,6 +82,43 @@ func main() {
 	}
 
 	defer connection.Close()
+
+	Save_dir = iniconf.String("save_dir")
+	if len(Save_dir) < 1 {
+		Save_dir = "/opt/nway/"
+	}
+	Save_rule = iniconf.String("save_rule")
+	if len(Save_rule) < 1 {
+		Save_rule = "caller_callee_uuid.wav"
+	}
+
+	//解析rule
+	s1 := strings.Split(Save_rule, "_")
+	if len(s1) < 2 {
+		Use_callee = false
+		Use_caller = false
+		Use_uuid = true
+	} else {
+		for i := 0; i <= len(s1); i++ {
+			if s1[i] == "caller" {
+				Use_caller = true
+			} else if s1[i] == "callee" {
+				Use_callee = true
+			} else if s1[i] == "uuid" {
+				Use_uuid = true
+			}
+		}
+		if Use_callee == false && Use_caller == false && Use_uuid == false {
+			Use_uuid = true
+		}
+	}
+	s2 := strings.Split(Save_rule, ".")
+	if len(s2) < 1 {
+		Ext_name = ".wav"
+	} else {
+		Ext_name = "." + s2[len(s2)-1]
+	}
+	//end 解析rule
 	buffer := make([]byte, 1024)
 	rand.Seed(time.Now().Unix())
 	Sessions = make(map[string]Rst_session, 0)
