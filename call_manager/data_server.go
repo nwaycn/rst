8da2f70e7e5066fc2a0a3025deb778d5b66ddcfe
lihao <lihao@nway.com.cn>
2024-08-08 13:46:08 +0800

    added check port

--- call_manager/data_server.go
+++ call_manager/data_server.go
@@ -1,3 +1,31 @@
 package call_manager
 
+import "net"
+
 //查找空闲的端口启用udp server用于接收裸数据
+// 检查端口是否可用
+func CheckUDPPort(port int) bool {
+    addr := net.UDPAddr{
+        Port: port,
+        IP:   net.ParseIP("0.0.0.0"),
+    }
+    conn, err := net.ListenUDP("udp", &addr)
+    if err != nil {
+        return false
+    }
+    conn.Close()
+    return true
+}
+// 检查TCP端口是否可用
+func CheckTCPPort(port int) bool {
+    addr := net.TCPAddr{
+        Port: port,
+        IP:   net.ParseIP("0.0.0.0"),
+    }
+    conn, err := net.ListenTCP("tcp", &addr)
+    if err != nil {
+        return false
+    }
+    conn.Close()
+    return true
+}
